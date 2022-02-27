c4e2be4b83fdd5fd513e4d8ce2ac6d9d7fb449d6
Tushan Shahid <63444838+tushan-shahid@users.noreply.github.com>
2022-02-26 23:34:12 -0800

    updated write method
    completed the write method in textFiles.java

--- src/nova/TextFiles.java
+++ src/nova/TextFiles.java
@@ -8,7 +8,13 @@ public class TextFiles {
 	
 	public ArrayList<String> read() {
 		ArrayList<String> keywords = new ArrayList<String>();
-		DataInputStream din = new DataInputStream(new FileInputStream("keywords.txt"));
+		DataInputStream din = null;
+		try {
+			din = new DataInputStream(new FileInputStream("keywords.txt"));
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+		}
 		Scanner s = new Scanner(din);
 		while (s.hasNext()){
 		    keywords.add(s.next());
@@ -19,9 +25,34 @@ public class TextFiles {
 	
 	public void write() {
 		
+		User response = new User();
+		
+		 try {
+		      File userDialogue = new File("userDialogue.txt");
+		      if (userDialogue.createNewFile()) {
+		        System.out.println("User Dialogue txt file created: " + userDialogue.getName());
+		      } else {
+		       
+		        try {
+				      FileWriter dialogueWriter = new FileWriter("userDialogue.txt");
+				      dialogueWriter.write(response.getUserResponse());
+				      dialogueWriter.close();
+				    } catch (IOException e) {
+				      System.out.println("An error occurred.");
+				      e.printStackTrace();
+				    }
+		      }
+		    } catch (IOException e) {
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		    }
+		 
+		
 	}
 	
 	public String getKeyword() {
+		
+		
 		return null;
 	}
 	
