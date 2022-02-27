8d18098679b736b275a7aabe1224e854bc54e0fc
Luca deVerteuil <lucadeverteuil@gmail.com>
2022-02-27 13:58:16 -0800

    Added key emotions to user
    If the user shows that they are angry but finding key words in the sentence then a text saying: "we are sorry for any stress this might have caused you. I added a text file and edited the User file

--- src/nova/User.java
+++ src/nova/User.java
@@ -7,6 +7,10 @@ public class User{
 	String selectPrompt;
 	
 	int setRating;
+
+	String UserEmotion ; 
+	
+	String AngerKeyWords [] ;
 	
 	public String getUserResponse() {
 		return userResponse;
@@ -32,5 +36,49 @@ public class User{
 		this.setRating = setRating;
 	}
 
+	public String getEmotions(){
+		return UserEmotion ; 
+	}
+
+	public void FindEmotion(String userresponse) 
+{
+	String Response [] = userresponse.split(" ");
+
+	try 
+	{
+		// this get the file emotions text file //
+		File emt = new File("emotions.txt");
+		
+		// this creates a scanner to be bale to read the text file //
+		Scanner Reader = new Scanner(emt);
+		
+		// Creates an Array List //
+		List<String> lines = new ArrayList<String>();
+		
+		// Puts the text File into The Array List //
+		while (Reader.hasNextLine()) {
+		  lines.add(Reader.nextLine());
+		}
+		// Puts the Array list into a simple Array // 
+		AngerKeyWords = lines.toArray(new String[0]);
+		
+		// this is a for loop to see if the users input is equal to any of the adjectives in the emotions.txt document // 
+		for (int i = 0; i < Response.length; i++) 
+		{
+			
+			for(int p =0 ; p <AngerKeyWords.length; p++) 
+			{
+				if(Response[i].equalsIgnoreCase(AngerKeyWords[p])) 
+				{
+					// If There is a match then it will say that the user is angry //
+					UserEmotion = "We are sorry for any stress or incovince this may cause you" ;
+				}
+			}
+		}
+	}catch(FileNotFoundException e) {
+		System.out.println("File was not found");
+	}
+	
+}
 	
 }
