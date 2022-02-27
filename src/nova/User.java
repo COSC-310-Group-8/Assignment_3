ba3658181bdd12b17ea1d6c198c87a6275d70ea6
Tushan Shahid <63444838+tushan-shahid@users.noreply.github.com>
2022-02-26 23:31:36 -0800

    added setters and getters
    added setters and getter methods

--- src/nova/User.java
+++ src/nova/User.java
@@ -1,10 +1,36 @@
 package nova;
 
-public class User {
-	String userResponse;
+public class User{
 	
+	String userResponse;
+
 	String selectPrompt;
 	
 	int setRating;
+	
+	public String getUserResponse() {
+		return userResponse;
+	}
+
+	public void setUserResponse(String userResponse) {
+		this.userResponse = userResponse;
+	}
+
+	public String getSelectPrompt() {
+		return selectPrompt;
+	}
+
+	public void setSelectPrompt(String selectPrompt) {
+		this.selectPrompt = selectPrompt;
+	}
+
+	public int getSetRating() {
+		return setRating;
+	}
+
+	public void setSetRating(int setRating) {
+		this.setRating = setRating;
+	}
+
 	
 }
