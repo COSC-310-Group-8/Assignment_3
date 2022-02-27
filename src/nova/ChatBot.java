6aa4f4c8456f975ee54f2509e44eb3c900ab197e
Tushan Shahid <63444838+tushan-shahid@users.noreply.github.com>
2022-02-26 23:38:02 -0800

    updated Chatbot.java with setters and getters
    added setters and getters

--- src/nova/ChatBot.java
+++ src/nova/ChatBot.java
@@ -1,6 +1,7 @@
 package nova;
 
 public class ChatBot {
+	
 	String greeting;
 	
 	String promptUser;
@@ -10,4 +11,46 @@ public class ChatBot {
 	String conclude;
 	
 	int getRating;
+	
+	public String getGreeting() {
+		return greeting;
+	}
+
+	public void setGreeting(String greeting) {
+		this.greeting = greeting;
+	}
+
+	public String getPromptUser() {
+		return promptUser;
+	}
+
+	public void setPromptUser(String promptUser) {
+		this.promptUser = promptUser;
+	}
+
+	public String getResponse() {
+		return response;
+	}
+
+	public void setResponse(String response) {
+		this.response = response;
+	}
+
+	public String getConclude() {
+		return conclude;
+	}
+
+	public void setConclude(String conclude) {
+		this.conclude = conclude;
+	}
+
+	public int getGetRating() {
+		return getRating;
+	}
+
+	public void setGetRating(int getRating) {
+		this.getRating = getRating;
+	}
+
+
 }
